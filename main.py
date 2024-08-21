eb3fd4876d86a2674e57e7f85ff90d2972999497
yuriaxiv <yuriamods@gmail.com>
2024-08-21 21:36:54 +1200

    Add session states

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import json
-import os
-
 import streamlit as st
+import extra_streamlit_components as stx
 
 st.markdown(
     """
@@ -45,6 +44,29 @@ def load_all_data():
 
 
 ALL_DATA = load_all_data()
+
+# Cookie manager
+cookie_manager = stx.CookieManager()
+
+
+# Helper function to get cookie value or default
+def get_cookie(key, default):
+    value = cookie_manager.get(key)
+    return value if value is not None else default
+
+
+# Helper function to set cookie with unique key
+def set_cookie(key, value):
+    cookie_manager.set(key, value, key=f'set_{key}')
+
+
+# Initialize state from cookies
+st.session_state.gender = get_cookie('gender', "Female")
+st.session_state.race = get_cookie('race', races_list[0])
+st.session_state.part_type = get_cookie('part_type', part_types[0])
+st.session_state.body_type = get_cookie('body_type', body_types_female[0])
+st.session_state.face_number = int(get_cookie('face_number', '1'))
+st.session_state.texture_type = get_cookie('texture_type', texture_types[0])
 
 
 @st.dialog("Gen3")
@@ -108,32 +130,48 @@ def get_valid_face_numbers(race, gender):
 
 
 # Streamlit App
-st.title("XIV Texture Path Generator")
+st.title("XIV Texture Path Generator", anchor=False)
 
 # First row
 col1, col2 = st.columns(2)
 with col1:
-    gender = st.selectbox("Gender", options=["Female", "Male"], index=0)
+    gender = st.selectbox("Gender", options=["Female", "Male"], index=["Female", "Male"].index(st.session_state.gender))
+    if gender != st.session_state.gender:
+        st.session_state.gender = gender
+        set_cookie('gender', gender)
 
 with col2:
-    race_options = races_list
-    race = st.selectbox("Race", options=race_options, index=0)
+    race = st.selectbox("Race", options=races_list, index=races_list.index(st.session_state.race))
+    if race != st.session_state.race:
+        st.session_state.race = race
+        set_cookie('race', race)
 
 # Second row
 col3, col4, col5 = st.columns(3)
 with col3:
-    part_type = st.selectbox("Part Type", options=part_types, index=0)
+    part_type = st.selectbox("Part Type", options=part_types, index=part_types.index(st.session_state.part_type))
+    if part_type != st.session_state.part_type:
+        st.session_state.part_type = part_type
+        set_cookie('part_type', part_type)
 
 with col4:
     if part_type == "Body":
         body_types = body_types_female if gender == "Female" else body_types_male
-        body_type = st.selectbox("Body Type", options=body_types, index=0)
+        body_type = st.selectbox("Body Type", options=body_types, index=body_types.index(
+            st.session_state.body_type) if st.session_state.body_type in body_types else 0)
+        if body_type != st.session_state.body_type:
+            st.session_state.body_type = body_type
+            set_cookie('body_type', body_type)
         face_number = None
     else:
         body_type = None
         if part_type in ["Face", "Eyes", "Brow/Lash"]:
             valid_face_numbers = get_valid_face_numbers(race, gender)
-            face_number = st.selectbox("Face Number", options=valid_face_numbers, index=0)
+            face_number = st.selectbox("Face Number", options=valid_face_numbers, index=valid_face_numbers.index(
+                st.session_state.face_number) if st.session_state.face_number in valid_face_numbers else 0)
+            if face_number != st.session_state.face_number:
+                st.session_state.face_number = face_number
+                set_cookie('face_number', str(face_number))
         else:
             face_number = None
 
@@ -161,16 +199,23 @@ with col5:
     reversed_display_names = {v: k for k, v in display_names.items()}
 
     # Get the selected texture type from the dropdown
-    selected_display_name = st.selectbox("Texture", options=list(display_names.values()), index=0)
+    selected_display_name = st.selectbox("Texture", options=list(display_names.values()),
+                                         index=list(display_names.values()).index(display_names[
+                                                                                      st.session_state.texture_type]) if st.session_state.texture_type in display_names else 0)
 
     # Map the selected display name back to the original texture type
     texture_type = reversed_display_names.get(selected_display_name, "Diffuse/Base")
+
+    # Update texture_type in session state and cookie
+    if st.session_state.texture_type != texture_type:
+        st.session_state.texture_type = texture_type
+        set_cookie('texture_type', texture_type)
 
 # Generate the path based on selections
 generated_path = get_texture_path(gender, race, part_type, body_type, texture_type, face_number)
 
 # Display the generated path
-st.subheader("Path:")
+st.subheader("Path:", anchor=False)
 st.code(generated_path, language="text")
 
 # Extra notes dialogs
